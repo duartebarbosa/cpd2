131c8ba3dc2a02cf1657c2dfa291981e945c5d92
Utilizador Público <public@HOST>
2013-10-18 18:50:15 +0100

    Added wolves fighting.

--- wolves-squirrels-serial.c
+++ wolves-squirrels-serial.c
@@ -105,7 +105,7 @@ void move(world_cell* cell, world_cell* dest_cell) {
 	} else if((cell->type == SQUIRREL || cell->type == SQUIRREL_IN_TREE) && dest_cell->type == SQUIRREL){
 		/* Squirrel moving to squirrel*/
 		dest_cell->type = cell->type;
-		dest_cell->breeding_period = (cell->breeding_period > dest_cell->breeding_period ? cell->breeding_period : dest_cell->breeding_period)+1; /* Should we increment the breeding period? */
+		dest_cell->breeding_period = (cell->breeding_period > dest_cell->breeding_period ? cell->breeding_period : dest_cell->breeding_period)+1; /*FIXME: Should we increment the breeding period? */
 		dest_cell->starvation_period = cell->starvation_period;
 		
 		/* clean cell */
@@ -169,8 +169,14 @@ void move(world_cell* cell, world_cell* dest_cell) {
 	} else if(cell->type == WOLF && dest_cell->type == WOLF){
 		/* Wolf fighting wolf */
 		dest_cell->type = cell->type;
-		dest_cell->breeding_period = cell->breeding_period;
-		dest_cell->starvation_period = cell->starvation_period;
+		
+		if(cell->starvation_period == dest_cell->starvation_period){
+			dest_cell->breeding_period = (cell->breeding_period > dest_cell->breeding_period ? cell->breeding_period : dest_cell->breeding_period)+1; /*FIXME: Should we increment the breeding period? */
+			/* same starvation */
+		} else {
+			dest_cell->breeding_period = (cell->starvation_period > dest_cell->starvation_period ? cell->breeding_period : dest_cell->breeding_period)+1; /*FIXME: Should we increment the breeding period? */
+			dest_cell->starvation_period = (cell->starvation_period > dest_cell->starvation_period ? cell->starvation_period : dest_cell->starvation_period)+1;
+		}
 		
 		/* clean cell */
 		cell->type = EMPTY;
@@ -179,13 +185,20 @@ void move(world_cell* cell, world_cell* dest_cell) {
 	} else if(cell->type == WOLF){
 		/* simple Wolf */
 		dest_cell->type = cell->type;
-		dest_cell->breeding_period = cell->breeding_period;
-		dest_cell->starvation_period = cell->starvation_period;
+		dest_cell->breeding_period = cell->breeding_period+1;
+		dest_cell->starvation_period = cell->starvation_period-1;
 		
-		/* clean cell */
-		cell->type = EMPTY;
-		cell->breeding_period = 0;
-		cell->starvation_period = 0;
+		/* clean cell or reproduce*/
+		if(dest_cell->breeding_period >= wolf_breeding_period){
+			cell->type = WOLF;
+			cell->breeding_period = 0;
+			cell->starvation_period = wolf_starvation_period;
+		} else {
+			cell->type = EMPTY;
+			cell->breeding_period = 0;
+			cell->starvation_period = 0;
+		}
+
 	} else {
 		exit(3); 
 	}
